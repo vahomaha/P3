6cafda62e5006b4f8fa260b3ff4f5e83ac2a4445
vahomaha <123481505+vahomaha@users.noreply.github.com>
2023-02-21 10:25:44 -0500

    Update Testuserprofile.java

--- src/testuserprofile/Testuserprofile.java
+++ src/testuserprofile/Testuserprofile.java
@@ -2,17 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+//created a package
 package testuserprofile;
 
 /**
  *
  * @author DELL
  */
+
+//created a class
 public class Testuserprofile {
 
     /**
      * @param args the command line arguments
      */
+    
+    //create main method
     public static void main(String[] args) {
         // TODO code application logic here
     }
